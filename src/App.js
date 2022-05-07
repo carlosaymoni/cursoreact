d7fd2161e72490b94f1a9273694af32d2a4a6b84
Carlos Aymoni <carlosaymoni@gmail.com>
2022-05-06 22:17:32 -0300

    Clase de instalacion y configuracion del entorno

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
+const AvatarJSX = (props) => {
+  const src = `https://randomuser.me/api/portraits/lego/${props.id}.jpg`
+
+  return (
+      <picture>
+          <img src={src} alt='Lego 1' className='img1'/>
+          { props.name}
+      </picture>
+  )
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <AvatarJSX id={2} name={"CarlosA"}/>
+        <AvatarJSX id={3} name={"Lucas"}/>
+        <AvatarJSX id={4} name={"Leandro"}/>
+
       </header>
     </div>
   );
